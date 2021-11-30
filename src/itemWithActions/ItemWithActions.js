2cee3c41e7ae019040226ab1849e48abac7588e5
Tyler Hawkins <tylerh@adobe.com>
2021-11-30 15:49:08 -0700

    feat: adds contextual label to action buttons

--- src/itemWithActions/ItemWithActions.js
+++ src/itemWithActions/ItemWithActions.js
@@ -19,10 +19,18 @@ export const ItemWithActions = ({ title, onClick, onEdit, onDelete }) => {
       >
         <div>{title}</div>
         <div className="actionsContainer">
-          <button className="button outline" onClick={onEdit}>
+          <button
+            className="button outline"
+            onClick={onEdit}
+            aria-label={`Edit item: ${title}`}
+          >
             Edit
           </button>
-          <button className="button underline" onClick={onDelete}>
+          <button
+            className="button underline"
+            onClick={onDelete}
+            aria-label={`Delete item: ${title}`}
+          >
             Delete
           </button>
         </div>
